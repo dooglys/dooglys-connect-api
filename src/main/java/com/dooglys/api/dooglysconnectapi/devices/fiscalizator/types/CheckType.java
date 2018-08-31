c1013e9568104834f093719ed000a4a02f0d0e51
Vitt0ri0 <vityaiiteen@gmail.com>
2018-08-31 17:59:17 +0300

    DGC-00 checkType fix

--- src/main/java/com/dooglys/api/dooglysconnectapi/devices/fiscalizator/types/CheckType.java
+++ src/main/java/com/dooglys/api/dooglysconnectapi/devices/fiscalizator/types/CheckType.java
@@ -4,5 +4,9 @@ package com.dooglys.api.dooglysconnectapi.devices.fiscalizator.types;
  * Created by aldom on 28.12.2017.
  */
 public enum CheckType {
-    SELL, RETURN, BUY, RETURN_BUY, CORRECTION
+    SELL,
+    RETURN,
+    BUY,
+    CORRECTION,
+    RETURN_BUY
 }
